fa66a06473c813af4f5112f54983d96b64f28eae
Shashank-btc <89379408+Shashank-btc@users.noreply.github.com>
2023-11-11 00:53:24 +0530

    Normal part 2 program 11/11

---
+++ Data Structure/src/com/HashingProg/QuadarticHashingOA/Main.java
@@ -0,0 +1,15 @@
+package com.HashingProg.QuadarticHashingOA;
+
+class Main {
+  public static void main(String[] args) {
+    QuadraticProbing quadraticProbing = new QuadraticProbing(13);
+    quadraticProbing.insertKeyInHashTable("The");
+		quadraticProbing.insertKeyInHashTable("quick");
+		quadraticProbing.insertKeyInHashTable("brown");
+		quadraticProbing.insertKeyInHashTable("fox");
+		quadraticProbing.insertKeyInHashTable("over");
+		quadraticProbing.insertKeyInHashTable("lazy");
+		quadraticProbing.displayHashTable();
+
+  }
+}
